87d5d4a3e5e0dc44fb591cec110fd0a200b46752
rhiokim <rhio.kim@gmail.com>
2011-09-09 16:19:04 +0900

    add gzip adapter

---
+++ test/gzip-test.js
@@ -0,0 +1,8 @@
+var adapter = require('../src/gzip').Gzip;
+var gzip = new adapter();
+
+gzip.on('complete', function(stdout){
+   console.log('gzip completed');
+});
+gzip.compress('../example/www/js/TodoView.js');
+
